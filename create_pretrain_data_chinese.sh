c18efd0d2b43e187f4d7db0d9bb7fc290c9edd86
b <brightmart@hotmail.com>
2020-02-07 22:50:20 +0800

    update

--- create_pretrain_data_chinese.sh
+++ create_pretrain_data_chinese.sh
@@ -11,11 +11,12 @@ fi
 
 for((i=$1;i<=$2;i++));
 do
-  random_seed=`expr 12345 + $i `
-  echo $random_seed
+  #random_seed=`expr 12345 + $i `
+  # echo $random_seed
   python3 create_pretraining_data.py --input_file=$PWD/data/news_zh_$i.txt \
     --output_file=$PWD/tf_records_all/tf_news2016zh_$i.tfrecord --vocab_file=$PWD/bert_base_dir/chinese_L-12_H-768_A-12/vocab_sim_7895.txt \
-    --do_lower_case=True --max_seq_length=512 --max_predictions_per_seq=51 --masked_lm_prob=0.10  --random_seed=$random_seed >$PWD/log/tfrecord_$i.log 2>&1 &
+    --do_lower_case=True --max_seq_length=512 --max_predictions_per_seq=51 --masked_lm_prob=0.10  >$PWD/log/tfrecord_$i.log 2>&1 &
+    # --random_seed=$random_seed
   j=$[j+1]
   if [ $j -eq 2 ];then
     wait
