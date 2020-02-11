8ad3e5edbbccc3db21386c32c85c437b08687165
b <brightmart@hotmail.com>
2020-02-11 17:13:19 +0800

    update

--- run_pretraining_c5_vocab8k_3g.sh
+++ run_pretraining_c5_vocab8k_3g.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 BERT_BASE_DIR=./bert_base_dir/chinese_L-12_H-768_A-12
 CURRENT_TIME=$(date "+%Y%m%d-%H%M%S")
-INPUT_DIR=gs://clue_pretrain_corpus/tfrecords/bert_base_128_c5_vocab2w/clue_pretrain128_*.tfrecord
-OUTPUT_DIR=gs://clue_pretrain_corpus/experiments/bert_base_128_c5_vcoab2w_3g/$CURRENT_TIME
+INPUT_DIR=gs://clue_pretrain_corpus/tfrecords/bert_base_128_c5_vocab8k_3g/clue_pretrain128_*.tfrecord
+OUTPUT_DIR=gs://clue_pretrain_corpus/experiments/bert_base_128_c5_vcoab8k_3g/$CURRENT_TIME
 IP=10.240.1.26
 nohup python3 run_pretraining.py \
   --use_tpu=True  --tpu_name=grpc://${IP}:8470 --tpu_zone=us-central1-a \
