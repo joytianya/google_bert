0fdaa235f7424d01d4cfb9254dd1c1beb20ed7db
b <brightmart@hotmail.com>
2020-02-20 23:34:51 +0800

    update

--- tpu/run_classifier_c5_voc8k_100g_tnews.sh
+++ tpu/run_classifier_c5_voc8k_100g_tnews.sh
@@ -50,6 +50,6 @@ run_task() {
 # bert_base_128_c5_vcoab8k_3g
 ##command##task_name##model_name##max_seq_length##train_batch_size##learning_rate##num_train_epochs##save_checkpoints_steps##tpu_ip
 #run_task afqmc bert_base_128_c5_vcoab8k_3g 128 16 2e-5 3 300 10.240.1.34 # DOING #
-run_task tnews best_checkpoint_444k 128 16 2e-5 3 1000 10.240.1.26 #
+run_task tnews best_checkpoint_444k 128 16 2e-5 3 1000 10.240.1.18 #
 #run_task iflytek bert_base_128_c5_vcoab8k_3g 128 32 2e-5 3 300 10.240.1.2
 #run_task cmnli bert_base_128_c5_vcoab8k_3g 128 64 3e-5 2 1400 10.240.1.10
