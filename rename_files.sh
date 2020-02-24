ed0fbdca5ec2d46116ae3f4f010ff5a6eff638b3
b <brightmart@hotmail.com>
2020-02-24 17:28:42 +0800

    update

--- rename_files.sh
+++ rename_files.sh
@@ -3,9 +3,9 @@
 i=0
 for file in `gsutil ls gs://raw_text/pretrain_data_test`
 do
- echo file
+ echo $file
  printf -v l "%07d" $i
  echo $l
- gsutil mv file gs://raw_text/pretrain_data_test_continue/clue_corpus_classic_$l.txt
+ gsutil mv $file gs://raw_text/pretrain_data_test_continue/clue_corpus_classic_$l.txt
  i=$[i+1]
 done
