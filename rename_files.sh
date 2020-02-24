ed004ae32a25ac7dd8c9aa785704b451338868d3
b <brightmart@hotmail.com>
2020-02-24 17:47:39 +0800

    update

--- rename_files.sh
+++ rename_files.sh
@@ -3,9 +3,14 @@
 i=0
 for file in `gsutil ls gs://raw_text/pretrain_data_train`
 do
- echo $file
+ echo ' file is $file'
  printf -v l "%07d" $i
- echo $l
- gsutil -m mv $file gs://raw_text/pretrain_data_train_continue/clue_corpus_classic_$l.txt
+ #echo $l
+ remainder=$i % 100
+ echo "remainder is $remainder"
+ if [ "$remainder" -eq 0 ]; then
+   gsutil -m mv $file gs://raw_text/pretrain_data_test_continue
+ else
+   gsutil -m mv $file gs://raw_text/pretrain_data_train_continue/clue_corpus_classic_$l.txt
  i=$[i+1]
 done
