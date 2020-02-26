963b5a0b0da1e0f96d34674863cdba49c4d6cca0
b <brightmart@hotmail.com>
2020-02-26 23:19:07 +0800

    update

--- tpu/run_classifier_c5_voc8k_100g_wsc_kdxf.sh
+++ tpu/run_classifier_c5_voc8k_100g_wsc_kdxf.sh
@@ -57,4 +57,4 @@ run_task() {
 #run_task tnews bert_base_128_c5_vcoab8k_3g 128 16 2e-5 3 300 10.240.1.10 #
 #run_task iflytek bert_base_128_c5_vcoab8k_3g 128 32 2e-5 3 300 10.240.1.2
 
-run_task wsc roberta-large-wwm-kdxf 128 8 1e-5 50 300 10.240.1.10
+run_task wsc roberta-large-wwm-kdxf 128 8 1e-5 50 300 10.240.1.2
