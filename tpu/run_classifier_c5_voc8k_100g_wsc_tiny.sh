2ea100a0f57c4253a63d1cea7f6c432af17b936f
b <brightmart@hotmail.com>
2020-02-27 17:33:42 +0800

    update

--- tpu/run_classifier_c5_voc8k_100g_wsc_tiny.sh
+++ tpu/run_classifier_c5_voc8k_100g_wsc_tiny.sh
@@ -3,10 +3,11 @@ CURRENT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 export PYTHONPATH=$CURRENT_DIR/../../:$PYTHONPATH
 CURRENT_TIME=$(date "+%Y%m%d-%H%M%S")
 CLUE_DATA_DIR=gs://clue_pretrain_corpus/experiments/public_data # 数据文件 no change
-# CLUE_PREV_TRAINED_MODEL_DIR=gs://clue_pretrain_corpus/experiments/fine_tuning/best_checkpoint_500k_roberta_tiny_clue/cmnli/20200221-102500 # 模型文件 no change
+#CLUE_PREV_TRAINED_MODEL_DIR=gs://clue_pretrain_corpus/experiments/roberta-large-clue-vocab8k-bsz512-v1-0221
 # CLUE_PREV_TRAINED_MODEL_DIR=gs://clue_pretrain_corpus/experiments/fine_tuning/best_checkpoint_512_additional_30g_294k/cmnli/20200226-103139 # (cmnli init)
 CLUE_PREV_TRAINED_MODEL_DIR=gs://clue_pretrain_corpus/experiments/roberta-large-clue-vocab8k-tiny-bsz512-v2-0226
-# gs://clue_pretrain_corpus/experiments/roberta-large-clue-vocab8k-v3-0219 # 模型文件 no change
+
+#gs://clue_pretrain_corpus/experiments/roberta-large-clue-vocab8k-v3-0219 # 模型文件 no change
 CLUE_OUTPUT_DIR=gs://clue_pretrain_corpus/experiments/fine_tuning # 产出的文件 no change
 
 run_task() {
@@ -52,9 +53,8 @@ run_task() {
 }
 # bert_base_128_c5_vcoab8k_3g
 ##command##task_name##model_name##max_seq_length##train_batch_size##learning_rate##num_train_epochs##save_checkpoints_steps##tpu_ip
-# run_task afqmc best_checkpoint_cmnli_1wsteps 128 16 4e-5 5 600 10.240.1.2 # DOING ###
-run_task afqmc best_checkpoint_roberta_tiny_clue_with_130g 128 16 2e-5 5 600 10.240.1.2 # DOING ###
+#run_task afqmc bert_base_128_c5_vcoab8k_3g 128 16 2e-5 3 300 10.240.1.34 # DOING #
+#run_task tnews bert_base_128_c5_vcoab8k_3g 128 16 2e-5 3 300 10.240.1.10 #
+#run_task iflytek bert_base_128_c5_vcoab8k_3g 128 32 2e-5 3 300 10.240.1.2
 
-#run_task tnews bert_base_128_c5_vcoab8k_3g 128 16 2e-5 3 300 10.240.1.34 #
-#run_task iflytek bert_base_128_c5_vcoab8k_3g 128 32 2e-5 3 300 10.240.1.26
-#run_task cmnli bert_base_128_c5_vcoab8k_3g 128 64 3e-5 2 1400 10.240.1.10
+run_task wsc best_checkpoint_roberta_tiny_clue_with_130g 128 8 2e-5 50 300 10.240.1.10
